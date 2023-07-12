98fd72c2ab51b35cd74c3f03d96c139db939d020
condrici <condrici@gmail.com>
2023-07-13 01:52:35 +0200

    Refactor core scraper functionality

--- api.py
+++ api.py
@@ -1,6 +1,14 @@
-from flask import Flask, jsonify, request, send_file, render_template
+import flask_api.status
+import logging
+from flask import Flask, jsonify, render_template
+from modules.DataScraper import PriceScraper
+from modules import Utilities
 
 app = Flask(__name__)
+
+####################
+# CONFIGURATION
+####################
 
 
 @app.errorhandler(404)
@@ -13,14 +21,24 @@ def not_found(e):
 if __name__ != '__main__':
     raise Exception("Could not start server due to unknown application name")
 
+Utilities.initiate_logging()
+
 ####################
 # HTTP ROUTES
 ####################
 
 
-@app.route('/test', methods=['GET'])
-def index():
-    return "Test page"
+@app.route('/scraper/<path:url>/<algorithm>', methods=['GET'])
+def index(url: str, algorithm: str):
+    try:
+        scraped_price = PriceScraper(url).scrape(algorithm).get_price_whole_with_decimal()
+    except BaseException as Ex:
+        message = 'Something went wrong'
+        Utilities.log(message + ' ' + str(Ex), logging.DEBUG)
+        return jsonify({'message': message}), flask_api.status.HTTP_500_INTERNAL_SERVER_ERROR
+
+    return jsonify({'message': scraped_price}), flask_api.status.HTTP_200_OK
+
 
 ####################
 # START APPLICATION
--- modules/DataScraper.py
+++ modules/DataScraper.py
@@ -1,24 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 
 class Price:
-    def __init__(self, price_integer: str, price_decimal_point_with_number: str):
-        self.price_integer = price_integer
-        self.price_decimal_point_with_number = price_decimal_point_with_number
+    format_comma_decimal_dot_thousands: str = "comma_decimal_dot_thousands"
 
-    def get_price_full(self) -> str:
-        return self.price_integer + self.price_decimal_point_with_number
+    __regex_comma_decimal_dot_thousands: str = "^[0-9]{1,3}([.]{1}[0-9]{3})*([,]{1}[0-9]+){0,1}$"
+    __whole_number: int
+    __decimal_number: int | None
+    __decimal_separator: str = ','
 
-    def get_price_integer(self):
-        return self.price_integer
+    def __init__(self, price: str, price_format: str):
+        if price_format != self.format_comma_decimal_dot_thousands:
+            raise Exception("Unknown or unsupported price format")
 
-    def get_price_decimal_point_with_number(self):
-        return self.price_decimal_point_with_number
+        if not re.search(self.__regex_comma_decimal_dot_thousands, price):
+            raise ValueError("Price doesn't have expected format format_comma_decimal_dot_thousands")
+
+        decimal_number = price.split(",")[-1]
+
+        self.__whole_number = int(price.split(',')[0])
+        self.__decimal_number = int(decimal_number) if decimal_number.isnumeric() else None
+
+    # Type string is returned because decimal separator is a comma
+    def get_price_whole_with_decimal(self) -> str:
+        if isinstance(self.__decimal_number, int):
+            return str(self.__whole_number) + self.__decimal_separator + str(self.__decimal_number)
+
+        return str(self.__whole_number)
+
+    def get_price_whole(self) -> int:
+        return self.__whole_number
 
 
 class PriceScraper:
-
     algorithm_emag_product_page = 'emag_product_page'
 
     def __init__(self, uri):
@@ -28,12 +44,13 @@ class PriceScraper:
         if scrape_algorithm != self.algorithm_emag_product_page:
             raise ValueError('Unknown algorithm used')
 
-        return self.scrape_emag_product_page()
+        return self.__scrape_emag_product_page()
 
-    def scrape_emag_product_page(self):
+    def __scrape_emag_product_page(self) -> Price:
         raw_html = requests.get(self.uri)
         soup_data = BeautifulSoup(raw_html.content, "html.parser")
-        price_integer = soup_data.select("p.product-new-price")[0].contents[0].text
-        price_decimal_point_with_number = soup_data.select(".product-new-price sup")[0].text
-        return Price(price_integer, price_decimal_point_with_number)
+        price_string = \
+            soup_data.select("p.product-new-price")[0].contents[0].text + \
+            soup_data.select(".product-new-price sup")[0].text
 
+        return Price(price_string, Price.format_comma_decimal_dot_thousands)
