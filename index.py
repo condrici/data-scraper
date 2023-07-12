1f8937b8b669173c13989b963ed86265ebb9173b
condrici <condrici@gmail.com>
2023-07-12 13:52:24 +0200

    Remove index file

--- index.py
+++
@@ -1,5 +0,0 @@
-from modules.DataScraper import PriceScraper
-
-price_scraper = PriceScraper("https://...")
-price = price_scraper.scrape(PriceScraper.algorithm_emag_product_page).get_price_full()
-print(price)
