febef3853d8829a893585b4c6d0c27aad284f45c
condrici <condrici@gmail.com>
2023-07-14 16:31:46 +0200

    Update TestDataScraper tests

--- tests/unit/TestDataScraper.py
+++
@@ -1,6 +0,0 @@
-import unittest
-
-
-class TestPriceScraper(unittest.TestCase):
-    def test_scrape(self):
-        self.assertEqual('data scraper'.upper(), 'DATA SCRAPER')
---
+++ tests/unit/modules/TestDataScraper.py
@@ -0,0 +1,48 @@
+import unittest
+from unittest.mock import patch
+
+from modules.DataScraper import PriceScraper
+from modules.SearchAlgorithms import SearchAlgorithmFactory
+
+
+class TestPriceScraper(unittest.TestCase):
+
+    def test_scrape(self):
+
+        # Test with nonexistent algorithm
+        # The url parameter is not validated currently TODO: Validate URL
+
+        with self.assertRaises(ValueError) as context:
+            self.__get_price_scraper().scrape(
+                'https://some-url.com', 'nonexistent_algorithm'
+            )
+
+        self.assertEqual('Unknown algorithm used', str(context.exception))
+
+        # Test with original object and mocked dependencies
+
+        price_scraper = self.__get_price_scraper_with_mocked_dependencies()
+        self.assertEqual(price_scraper.scrape('test', 'test'), True)
+
+    """ Original scrape functionality """
+    """ Useful for testing internal exceptions due to invalid arguments """
+
+    def __get_price_scraper(self):
+        search_algorithm_factory = SearchAlgorithmFactory()
+        return PriceScraper(search_algorithm_factory)
+
+    """ Mocked price scraper """
+    """ Useful for mocking the actual HTTP request via get_price """
+
+    @patch('modules.SearchAlgorithms.SearchAlgorithmFactory')
+    @patch('modules.SearchAlgorithms.SearchAlgorithm')
+    def __get_price_scraper_with_mocked_dependencies(
+            self,
+            mock_search_algorithm_factory,
+            mock_search_algorithm
+    ):
+        mock_search_algorithm.get_price.return_value = True
+        mock_search_algorithm_factory.create.return_value = \
+            mock_search_algorithm
+
+        return PriceScraper(mock_search_algorithm_factory)
