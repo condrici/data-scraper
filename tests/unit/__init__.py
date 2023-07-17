6af81c6ebe0b830c1eefc590c691ce4aa1635543
condrici <condrici@gmail.com>
2023-07-17 18:34:14 +0200

    Fix tests not starting when running install script

---
+++ tests/unit/__init__.py
@@ -0,0 +1,4 @@
+from tests.unit.modules.TestSchema import *
+from tests.unit.modules.TestDataScraper import *
+from tests.unit.modules.TestPriceFormats import *
+from tests.unit.modules.TestSearchAlgorithms import *
