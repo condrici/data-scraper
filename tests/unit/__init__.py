1d2369ae0134a30a38f5a9d84a245f61e1473b04
condrici <condrici@gmail.com>
2023-07-20 13:33:28 +0200

    Add Http unit tests

--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -2,3 +2,4 @@ from tests.unit.modules.TestSchema import *
 from tests.unit.modules.TestDataScraper import *
 from tests.unit.modules.TestPriceFormats import *
 from tests.unit.modules.TestSearchAlgorithms import *
+from tests.unit.modules.TestHttp import *
---
+++ tests/unit/modules/TestHttp.py
@@ -0,0 +1,88 @@
+import unittest
+from unittest.mock import patch, MagicMock
+
+import requests.utils
+from requests import Response
+
+from modules.Http import HttpRequestSettings, HttpRequest, DEFAULT_USER_AGENT
+
+
+class TestHttpRequestSettings(unittest.TestCase):
+
+    def test_get_use_default_user_agent(self) -> None:
+        http_request_settings = HttpRequestSettings(
+            use_default_user_agent=True
+        )
+        self.assertIsInstance(http_request_settings, HttpRequestSettings)
+        self.assertEqual(
+            http_request_settings.get_use_default_user_agent(),
+            True
+        )
+
+
+class TestHttpRequest(unittest.TestCase):
+
+    def test_get_with_http_headers_user_agent(self) -> None:
+        request_settings = HttpRequestSettings(use_default_user_agent=True)
+        request_api_mock = self.__get_mock_request_api()
+        http_request = HttpRequest(request_api_mock, request_settings)
+
+        # Filled headers= with user-agent
+
+        http_response = http_request.get(
+            'http://example.com',
+            None,
+            headers={'user-agent': 'Mozilla'}
+        )
+
+        self.assertIsInstance(http_response, Response)
+        request_api_mock.get.assert_called_once_with(
+            url='http://example.com',
+            headers={'user-agent': 'Mozilla'},
+            params=None
+        )
+
+    def test_get_with_empty_http_headers(self) -> None:
+        request_settings = HttpRequestSettings(use_default_user_agent=True)
+        request_api_mock = self.__get_mock_request_api()
+        http_request = HttpRequest(request_api_mock, request_settings)
+
+        # Empty headers=
+
+        http_response = http_request.get(
+            'http://example.com',
+            None,
+            headers={}
+        )
+
+        request_api_mock.get.assert_called_once_with(
+            url='http://example.com',
+            headers={'user-agent': DEFAULT_USER_AGENT},
+            params=None
+        )
+        self.assertIsInstance(http_response, Response)
+
+    def test_get_with_unset_http_headers(self) -> None:
+        request_settings = HttpRequestSettings(use_default_user_agent=True)
+        request_api_mock = self.__get_mock_request_api()
+        http_request = HttpRequest(request_api_mock, request_settings)
+
+        # Unset headers=
+
+        http_response = http_request.get(
+            'http://example.com',
+            None,
+        )
+
+        request_api_mock.get.assert_called_once_with(
+            url='http://example.com',
+            headers={'user-agent': DEFAULT_USER_AGENT},
+            params=None
+        )
+        self.assertIsInstance(http_response, Response)
+
+    def __get_mock_request_api(self) -> MagicMock:
+        request_api = MagicMock()
+        request_api.get.return_value = Response()
+
+        return request_api
